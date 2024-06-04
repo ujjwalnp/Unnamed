13f032bfb44b812e2eaf153c33d588f665e790a7
ujjwalnp <dhakalujjwal99@gmail.com>
2024-06-04 21:46:41 +0545

    feat(server): add server script

---
+++ server/index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+
+/* EXPRESS SERVER */
+const server = express()
+
+/* BODY PARSER */
+server.use(express.json())
+
+/* CORS */
+server.use(cors())
+server.options('*', cors())
+
+/* DATABASE CONNECTION */
+mongoose.connect(process.env.MONGO_URI, {
+    autoIndex: true,
+  }).then(() => {
+    logger.info('Connected to the database')
+  })
+   .catch((err) => {
+    logger.error(`Error connecting to the database: ${err}`)
+  })
+
+/* ROUTES */
+server.use('/api', routes)
+
+/* SERVER LISTENING */
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => {
+  logger.info(`Server is running on port ${PORT}`)
+})
