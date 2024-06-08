9003e8dde4de797c21c5bf67b641b08f0c777116
ujjwalnp <dhakalujjwal99@gmail.com>
2024-06-08 20:45:17 +0545

    chore(server): import routes to index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
+
+const routes = require('./routes')
 
 /* EXPRESS SERVER */
 const server = express()
